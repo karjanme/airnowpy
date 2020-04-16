11a6f2a50924e6053a3b49141af86d6993c93214
Karl Jansen <6225628+karjanme@users.noreply.github.com>
2020-04-15 18:32:13 -0700

    Set version to 0.1.0 for initial release

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name="AirNowPy",
-    version="0.1",
+    version="0.1.0",
     description="Python Library for the AirNow API",
     long_description=long_description,
     long_description_content_type="text/markdown",
