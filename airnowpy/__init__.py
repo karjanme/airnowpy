b2142534969388cd35e8f2d0f7268cf299aa8574
jnsnkrllive <6225628+jnsnkrllive@users.noreply.github.com>
2020-06-12 10:33:04 -0700

    Export modules of the package

---
+++ airnowpy/__init__.py
@@ -0,0 +1,8 @@
+# AirNowPy
+# Copyright 2020 Karl Jansen
+# See LICENSE for details
+
+""" AirNow API Library for Python """
+from airnowpy.api import API
+from airnowpy.category import Category
+from airnowpy.observation import Observation
